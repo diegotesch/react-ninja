e196c8657d4e7aca24892075e05a892187a0466c
diegotesch <diego.tesch@gmail.com>
2020-02-21 14:23:46 -0300

    atributos className e htmlFor

--- 03-mod1-parte02/src/app.js
+++ 03-mod1-parte02/src/app.js
@@ -6,8 +6,10 @@ import Title from './title'
 const App = React.createClass({
   render: function () {
     return (
-      <div>
+      <div className='container'>
         <Title name='Diego Tesch' />
+        <label htmlFor='input'>Input</label>
+        <input type='text' id='input' />
       </div>
     )
   }
