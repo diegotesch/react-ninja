1cfdaf491c300eada219877a06f988bc70537348
diegotesch <diego.tesch@gmail.com>
2020-03-05 08:46:38 -0300

    renderizando componentes com funcoes puras

--- 03-mod1-parte02/src/app.js
+++ 03-mod1-parte02/src/app.js
@@ -7,9 +7,7 @@ const App = React.createClass({
   render: function () {
     return (
       <div className='container'>
-        <Title name='Diego' lastname={{ first: 'Te'}} />
-        <label htmlFor='input'>Input</label>
-        <input type='text' id='input' />
+        <Title name='Diego' />
       </div>
     )
   }
--- 03-mod1-parte02/src/title.js
+++ 03-mod1-parte02/src/title.js
@@ -2,21 +2,30 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-  getDefaultProps: function () {
-    return {
-      name: 'Desconhecido',
-      lastname: {
-        first: 'Sem',
-        second: 'Sobrenome'
-      }
-    }
-  },
-  render: function () {
-    return (
-      <h1>Olá {this.props.name + ' ' + this.props.lastname.first + this.props.lastname.second}</h1>
-    )
-  }
-})
+const Title = ({ name, lastname }) => (
+  <h1>Olá {`${name} ${lastname}`}!</h1>
+)
+
+Title.defaultProps = {
+  name: 'Desconhecido',
+  lastname: 'Sem Sobrenome'
+}
+
+// const Title = React.createClass({
+//   getDefaultProps: function () {
+//     return {
+//       name: 'Desconhecido',
+//       lastname: {
+//         first: 'Sem',
+//         second: 'Sobrenome'
+//       }
+//     }
+//   },
+//   render: function () {
+//     return (
+//       <h1>Olá {this.props.name + ' ' + this.props.lastname.first + this.props.lastname.second}</h1>
+//     )
+//   }
+// })
 
 export default Title
