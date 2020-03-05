21a6ad44024de8151f4b4909db26e4879c51fa99
diegotesch <diego.tesch@gmail.com>
2020-03-05 08:34:29 -0300

    outros tipos de dados via props

--- 03-mod1-parte02/src/app.js
+++ 03-mod1-parte02/src/app.js
@@ -7,7 +7,7 @@ const App = React.createClass({
   render: function () {
     return (
       <div className='container'>
-        <Title name='Diego' lastname='Tesch' />
+        <Title name='Diego' lastname={{ first: 'Te'}} />
         <label htmlFor='input'>Input</label>
         <input type='text' id='input' />
       </div>
