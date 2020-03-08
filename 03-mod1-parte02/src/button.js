76ef88494db8d8ceaf9463d44e012552a6542638
diegotesch <diego.tesch@gmail.com>
2020-03-08 10:51:52 -0300

    38 - propTypes

--- 03-mod1-parte02/src/button.js
+++ 03-mod1-parte02/src/button.js
@@ -3,7 +3,15 @@
 import React from 'react'
 
 const Button = ({ children, handleClick }) => (
-  <button className='main-button' onClick={handleClick}>{children}</button>
+  <button
+    className='main-button' onClick={handleClick}
+  >
+    {children}
+  </button>
 )
+
+Button.propTypes = {
+  handleClick: React.PropTypes.func.isRequired
+}
 
 export default Button
