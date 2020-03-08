69ed66270ebbc1ea9122b20f6a79167b4b092706
diegotesch <diego.tesch@gmail.com>
2020-03-08 12:40:36 -0300

    47 - criando a marcacao da aplicacao

--- 03-mod1-parte02 (APP GITHUB)/src/app.js
+++ 03-mod1-parte02 (APP GITHUB)/src/app.js
@@ -1,43 +1,48 @@
 'use strict'
 
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      checked: false,
-      showContent: false
-    }
-  }
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input type='text' placeholder='Digite o nome do usuário no GitHub' />
+    </div>
 
-  render () {
-    return (
-      <div>
-        <label>
-          <input
-            type='checkbox'
-            checked={this.state.checked}
-            onChange={e => {
-              this.setState({
-                checked: !this.state.checked
-              }, () => {
-                // função de callback executada após a mudança de estado
-                this.setState({
-                  showContent: this.state.checked
-                })
-              })
-            }}
-          /> Mostrar Conteúdo
-        </label>
+    <div className='user-info'>
+      <img src='https://avatars.githubusercontent.com/u/11755086?v=3' alt='' />
+      <h1>
+        <a href='https://github.com/diegotesch'>
+          Diego Tesch
+        </a>
+      </h1>
 
-        {this.state.showContent &&
-          <div>
-            Conteúdo
-          </div>}
+      <ul className='repos-info'>
+        <li>Repositórios: 28</li>
+        <li>Seguidores: 10</li>
+        <li>Seguindos: 10</li>
+      </ul>
+
+      <div className='actions'>
+        <button>Ver repositórios</button>
+        <button>Ver favoritos</button>
       </div>
-    )
-  }
-}
+
+      <div className='repos'>
+        <h2>Repositórios:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+      <div className='starred'>
+        <h2>Favoritos:</h2>
+        <ul>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+)
 
 export default App
